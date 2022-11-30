28b0962a6b41acaf9054c0bcae5533dbe1fcd0e2
Zyaaco <Zyaaco1@gmail.com>
2022-12-01 00:02:57 +0200

    reponsiveness issues fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  basePath: "/nextjs-pages",
+  assetPrefix: "/nextjs-pages",
 };
 
 module.exports = nextConfig;
