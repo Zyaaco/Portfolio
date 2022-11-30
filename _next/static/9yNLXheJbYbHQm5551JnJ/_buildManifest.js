4ac98d7c0a8ae346a605b317a7b3947afd733d06
Zyaaco <30415608+Zyaaco@users.noreply.github.com>
2022-11-30 22:50:22 +0000

    Deploying to gh-pages from @ Zyaaco/nextjs-pages@3d02e1dc17b50ea199c7068a8a8206d0d1fc65fe 🚀

---
+++ _next/static/9yNLXheJbYbHQm5551JnJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/670-f72930ed557d1edd.js","static/chunks/pages/index-6431decff78b0a07.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
