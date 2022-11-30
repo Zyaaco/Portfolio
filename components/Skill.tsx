3585110ec51268927b5039e6152cf0dca034f30a
Zyaaco <Zyaaco1@gmail.com>
2022-12-01 00:37:06 +0200

    fixed responsiveness

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 type Props = {
   directionLeft?: boolean;
@@ -10,14 +11,9 @@ type Props = {
 function Skill({ directionLeft, src, percent }: Props) {
   return (
     <div className="group relative flex cursor-pointer">
-      <img
-        initial={{
-          x: directionLeft ? -200 : 200,
-          opacity: 0,
-        }}
-        whileInView={{ x: 0, opacity: 1 }}
-        transition={{ duration: 1 }}
+      <Image
         src={src}
+        alt="Skill picture"
         className="rounded-full border border-gray-500 object-cover w-20 h-20 md:w-28 md:h-28 xl:w-32 xl:h-32 
         filter group-hover:grayscale transition duration-300 ease-in-out"
       />
