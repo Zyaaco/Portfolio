fa3da35dab18fabd12aead73232c2ef7b9e15b15
Zyaaco <Zyaaco1@gmail.com>
2022-12-03 18:36:04 +0200

    added my linked in

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  basePath: "/Portfolio",
+  assetPrefix: "/Portfolio",
 };
 
 module.exports = nextConfig;
