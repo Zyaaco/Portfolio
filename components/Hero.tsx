32d1e41058fbbd04776cb64eb0f97d8462739a79
Zyaaco <Zyaaco1@gmail.com>
2023-03-29 16:39:53 +0300

    test

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ type Props = {};
 function Hero({}: Props) {
   const [text, count] = useTypewriter({
     words: [
-      '"Hey, I am Orestass "',
+      '"Hey, I am Orestas"',
       ">Developer",
       "<Designer />",
       "Your-next-hire.tsx",
